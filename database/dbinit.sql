a25dd3f62c16e198359252789f98a1d7474c68a7
Robert Fischer <smokejumperit@gmail.com>
2017-02-16 09:53:51 -0500

    Put the indexes back in, since we are creating load by doing 10k users

--- database/dbinit.sql
+++ database/dbinit.sql
@@ -1,25 +1,33 @@
-
 CREATE TABLE IF NOT EXISTS users (
-    id BIGSERIAL NOT NULL,
+    id BIGSERIAL NOT NULL PRIMARY KEY,
     firstName VARCHAR NOT NULL,
-    middleName VARCHAR,
+    middleName VARCHAR NOT NULL,
     lastName VARCHAR NOT NULL,
-    title VARCHAR,
+    title VARCHAR NOT NULL,
     streetAddress VARCHAR NOT NULL,
     city VARCHAR NOT NULL,
     state VARCHAR NOT NULL,
     zipcode VARCHAR(5) NOT NULL,
-    phoneNumber VARCHAR,
-    age INTEGER);
+    phoneNumber VARCHAR NOT NULL,
+    age INTEGER NOT NULL);
+
+CREATE INDEX ON users (firstName);
+CREATE INDEX ON users (lastName);
+CREATE INDEX ON users (city);
+CREATE INDEX ON users (state);
+CREATE INDEX ON users (zipcode);
+CREATE INDEX ON users (age);
 
 CREATE TABLE IF NOT EXISTS interests (
     id BIGSERIAL NOT NULL,
-    name VARCHAR);
+    name VARCHAR NOT NULL UNIQUE);
 
 CREATE TABLE IF NOT EXISTS userInterests (
-    userid BIGINT NOT NULL,
-    interestid BIGINT NOT NULL
+    userid BIGINT NOT NULL REFERENCES users(id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    interestid BIGINT NOT NULL REFERENCES interests(id) ON DELETE RESTRICT ON UPDATE CASCADE
 );
+
+CREATE UNIQUE INDEX ON userInterests (userid, interestid);
 
 CREATE VIEW userView AS
     SELECT
